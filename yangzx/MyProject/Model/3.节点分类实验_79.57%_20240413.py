6157f4ec33c84f2365e3120a473bf5faa0578893
yangwohenmai <zongxianyang@outlook.com>
2024-05-21 13:24:55 +0800

    update

---
+++ yangzx/MyProject/Model/3.节点分类实验_79.57%_20240413.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GATConv
+import baostock as bs
+import os
+import sys
+# sys.path.append用于向环境变量中添加路径
+#sys.path.append('..\..')
+# 打印文件绝对路径（absolute path）
+#print (os.path.abspath(__file__))  
+# 打印文件父目录的父目录的路径（文件的上两层目录）
+#print (os.path.dirname(os.path.dirname( os.path.abspath(__file__) ))) 
+# 要调取其他目录下的文件。 需要在atm这一层才可以
+#BASE_DIR=  os.path.dirname(os.path.dirname(os.path.dirname( os.path.abspath(__file__) )))
+#print(BASE_DIR)
+# 将这个路径添加到环境变量中。
+#sys.path.append(BASE_DIR)
+# 打印当前环境变量包含的所有路径
+#print(sys.path)
+
+sys.path.append('..')
+from Data import StockPool
+from Data import StockData
+from Data import TrainData
+
+
+lg = bs.login()
+#stockPoolList = StockPool.GetStockPool('',False,'')
+#for code in StockPool.GetALLStockListBaostock().keys():
+stockPriceDic = StockData.GetStockPriceDWMBaostock('600000.SH', 0)
+data = TrainData.TrainDataInt(stockPriceDic)[0]
+split = int(len(stockPriceDic)*0.8)
+train_mask=[]
+test_mask=[]
+for i in range(0,len(stockPriceDic)):
+    if i < split:
+        train_mask.append(True)
+        test_mask.append(False)
+    else:
+        train_mask.append(False)
+        test_mask.append(True)
+
+#print(data.x)
+#print(data.y)
+#print(data.x[0].tolist())
+#print(data.y.tolist())
+#print(data.edge_index[0].tolist())
+#print(data.edge_index[1].tolist())
+#定义网络架构
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        #self.conv1 = GCNConv(dataset.num_features, 128)  #输入=节点特征维度，16是中间隐藏神经元个数
+        self.conv1 = GCNConv(7, 32)  #输入=节点特征维度，16是中间隐藏神经元个数
+        self.conv2 = GATConv(32, 64)
+        self.conv3 = GCNConv(64, 128)
+        self.conv4 = GATConv(128, 2)
+        self.conv5 = GATConv(256, 2)
+        #self.conv4 = GCNConv(64, dataset.num_classes)
+        
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        #x = F.sigmoid(x)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = self.conv3(x, edge_index)
+        x = F.relu(x)
+        x = self.conv4(x, edge_index)
+        return F.log_softmax(x, dim=1)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net().to(device)
+data = data.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0005)
+
+#模型训练
+model.train()
+for epoch in range(2000):
+    optimizer.zero_grad()
+    out = model(data.x.to(torch.float32), data.edge_index)    #模型的输入有节点特征还有边特征,使用的是全部数据
+    #loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])   #损失仅仅计算的是训练集的损失
+    loss = F.nll_loss(out[train_mask], data.y.to(torch.long)[train_mask])   #损失仅仅计算的是训练集的损失
+    loss.backward()
+    optimizer.step()
+#测试：
+model.eval()
+#test_predict = model(data.x, data.edge_index)[data.test_mask]
+test_predict = model(data.x.to(torch.float32), data.edge_index)[test_mask]
+max_index = torch.argmax(test_predict, dim=1)
+#test_true = data.y[data.test_mask]
+test_true = data.y.to(torch.long)[test_mask]
+correct = 0
+for i in range(len(max_index)):
+    if max_index[i] == test_true[i]:
+        correct += 1
+print('测试集准确率为：{}%'.format(correct*100/len(test_true)))
+
+
+
---
+++ 网络资料/B站教程/GNN-Tutorial-main/README.md
@@ -0,0 +1,18 @@
+### GNN论文
+1.【ICLR-2017 GCN】<br>
+[Semi-Supervised Classification with Graph Convolutional Networks](https://arxiv.org/abs/1609.02907)
+
+2.【NIPS-2017 GraphSAGE】<br>
+[Inductive Representation Learning on Large Graphs](https://arxiv.org/abs/1706.02216)
+
+3.【ICLR-2018 GAT】<br>
+[Graph Attention Networks](https://arxiv.org/abs/1710.10903)
+
+4.【KDD-2019 Cluster-GCN】<br>
+[Cluster-GCN: An Efficient Algorithm for Training Deep and Large Graph Convolutional Networks](https://arxiv.org/abs/1905.07953)
+
+5.【ICLR-2022 GATv2】<br>
+[How Attentive are Graph Attention Networks?](https://arxiv.org/abs/2105.14491)
+
+6.【WWW-2019 HAN】<br>
+[Heterogeneous Graph Attention Network](https://arxiv.org/abs/1903.07293)
